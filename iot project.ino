9486f010ebe9641e791e4976c8b02239d3b0a316
Charudatta-M <charumahajan9956@gmail.com>
2025-08-20 15:55:08 +0530

    Add files via upload

---
+++ iot project.ino
@@ -0,0 +1,43 @@
+#include<Wire.h> 
+#include<LiquidCrystal_I2C.h> 
+#include<DHT.h>
+
+LiquidCrystal_I2C lcd(0x20,4,5,6,0,1,2,3,7,NEGATIVE);
+DHT dht(8,DHT11);
+String tempMessage;
+String humMessage;
+
+
+void setup() {
+  lcd.begin(16,2);
+  lcd.clear();
+  lcd.setCursor(1,0);
+  lcd.print("DHT11 Sensor");
+  dht.begin();
+
+  Serial.begin(9600);
+  Serial.println("Temperature and Humidity Sensor");
+  
+
+}
+
+void loop() {
+  float h  = dht.readHumidity();
+  float t  = dht.readTemperature();
+  if (isnan(h) || isnan(t))
+  {
+    Serial.println("Failed to read from DHT Sensor!");
+    loop();
+  }
+  tempMessage = "Temp: " + String(t) + "C";
+  humMessage = "Hum: " + String(h) + "%";
+  Serial.print(tempMessage + "   ");
+  Serial.println(humMessage );
+  lcd.clear();
+  lcd.setCursor(1,0);
+  lcd.print(tempMessage);
+  lcd.setCursor(1,1);
+  lcd.print(humMessage);
+
+  delay(1000);
+}
